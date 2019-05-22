0305e284a1e36faf81ddeec22612696a4929108d
Harrypotterrrr <jiahaolin19971119@126.com>
2019-05-22 22:18:56 +0800

    update maps & location

---
+++ maps/org-locations.js
@@ -0,0 +1,17 @@
+var addressPoints = [
+  [
+    "Tongji University Jiading Campus",
+    31.2865184,
+    121.209314957818
+  ],
+  [
+    "University of California Merced",
+    37.3654534,
+    -120.423136318792
+  ],
+  [
+    "Beijing China",
+    39.906217,
+    116.3912757
+  ]
+];
